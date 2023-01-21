c3b2ee3b44f7d49bf3ed7bf38d90ac16315facfa
M-R-D-A <shouldntbespoken@gmail.com>
2023-01-21 14:19:53 +0700

    react complete guide from Maximilian Schwarzmüller

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoints) => (
+        <ChartBar
+          key={dataPoints.id}
+          value={dataPoints.value}
+           maxValue={null}
+          label={dataPoints.label}
+        />
+      ))}
+    </div>
+  );
+};
